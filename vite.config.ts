f1c32ed749fb7c444e5ca8687b02e7891bf2939c
shashankrp <shashankrp2@gmail.com>
2026-05-04 11:47:07 +0530

    updated information

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,11 @@ export default defineConfig(({ mode }) => {
 
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
-
+      base: process.env.VITE_BASE_PATH || '/fertilizerManagementFrontend',
       proxy: {
         // all /api requests go to backend
         '/api': {
-          target: 'http://localhost:3000',
+          target: process.env.VITE_API_URL || 'http://localhost:5000',
           changeOrigin: true,
           secure: false
         }
